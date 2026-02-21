df0334298598be490a9f7bdbdbfeacab20a00395
Josh Taylor <joshbtayloriii@gmail.com>
2026-02-20 22:24:05 -0600

    Add base index.html

--- src/app.py
+++ src/app.py
@@ -5,5 +5,40 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def landing_page():
-    return "test"
-#   return flask.render_template('index.html')
+    return flask.render_template("index.html")
+
+@app.route('/zones')
+def zones():
+    return "TODO"
+
+@app.route('/items')
+def items():
+    return "TODO"
+
+@app.route('/canisters')
+def canisters():
+    return "TODO"
+
+@app.route('/quests')
+def quests():
+    return "TODO"
+
+@app.route('/skills')
+def skills():
+    return "TODO"
+
+@app.route('/reputation')
+def reputation():
+    return "TODO"
+
+@app.route('/special_items')
+def special_items():
+    return "TODO"
+
+@app.route('/shops')
+def shops():
+    return "TODO"
+
+@app.route('/crafting')
+def crafting():
+    return "TODO"
