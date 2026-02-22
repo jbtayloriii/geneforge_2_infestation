89b88cf0f37892ba8a8767e8f5b0e55ba1155012
Josh Taylor <joshbtayloriii@gmail.com>
2026-02-21 22:32:42 -0600

    Add logic for displaying items

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
+"""Main entrypoint for the geneforge site."""
+import csv
+
+import objects
+import data_loader
+
 import flask
 
+
+
 app = flask.Flask(__name__)
+
+_DATA = data_loader.DataLoader()
 
 
 @app.route('/')
@@ -13,7 +23,18 @@ def zones():
 
 @app.route('/items')
 def items():
-    return "TODO"
+    return flask.render_template(
+        "items.html",
+        item_variety_by_id=_DATA.item_varieties_by_id,
+        item_templates_by_variety_id=_DATA.item_templates_by_variety_id
+    )
+
+@app.route('/item_template/<item_id>')
+def item_template(item_id):
+
+    int_id = int(item_id)
+    template = _DATA.item_templates_by_id[int_id] if int_id in _DATA.item_templates_by_id else None
+    return flask.render_template("item_template.html", template=template)
 
 @app.route('/canisters')
 def canisters():
